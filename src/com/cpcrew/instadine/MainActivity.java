b6a9d7d0e8e2208d2dea43cff3a953283b7cf5a8
Chintan Sheth <chintansheth87@gmail.com>
2014-07-05 19:04:58 -0700

    Added InstaDineApplication to hold all configs. Added Parse config. Added Internet access permission in the manifest file

--- src/com/cpcrew/instadine/MainActivity.java
+++ src/com/cpcrew/instadine/MainActivity.java
@@ -4,8 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.parse.Parse;
-import com.parse.ParseAnalytics;
+
 
 
 public class MainActivity extends Activity {
@@ -14,5 +13,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 }
